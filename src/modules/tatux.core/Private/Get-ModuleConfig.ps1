cd366cce0b773778c399e769a0bc0c3bc8bde3c6
Nigel Tatschner <ntatschner@gmail.com>
2024-06-28 12:05:30 +0100

    Updates

--- src/modules/tatux.core/Private/Get-ModuleConfig.ps1
+++ src/modules/tatux.core/Private/Get-ModuleConfig.ps1
@@ -3,7 +3,7 @@ function Get-ModuleConfig {
 
     param (
         [parameter(Mandatory)]
-        [ValidateNotNullOrWhiteSpace]
+        [ValidateNotNullOrWhiteSpace()]
         $CommandPath
     )
     
