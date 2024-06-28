50c24f70b8d73d3ab389b8b652ea8d3463b84187
Nigel Tatschner <ntatschner@gmail.com>
2024-06-28 14:37:24 +0100

    Fixing Dumbshit!

--- src/modules/tatux.core/Private/Get-ModuleConfig.ps1
+++ src/modules/tatux.core/Private/Get-ModuleConfig.ps1
@@ -9,12 +9,13 @@ function Get-ModuleConfig {
     try {
         $ModulePath = $(Split-Path -Path (Split-Path -Path $CommandPath -Parent) -Parent)
         $ModuleName = Get-ChildItem -Path $ModulePath -Filter "*.psd1" -File | Select-Object -First 1 | Select-Object -ExpandProperty BaseName
-        $UserPowerShellModuleConfigPath = Join-Path -Path $(Split-Path -Path $($env:PSModulePath -split ';' | ForEach-Object { if (($_ -match $([regex]::Escape($env:USERNAME))) -and ($_ -notmatch '\.')) {$_} }) -Parent) -ChildPath 'Config'
+        $UserPowerShellModuleConfigPath = Join-Path -Path $(Split-Path -Path $($env:PSModulePath -split ';' | ForEach-Object { if (($_ -match $([regex]::Escape($env:USERNAME))) -and ($_ -notmatch '\.')) { $_ } }) -Parent) -ChildPath 'Config'
         $ModuleConfigPath = Join-Path -Path $UserPowerShellModuleConfigPath -ChildPath $ModuleName
         $ModuleConfigFilePath = Join-Path -Path $ModuleConfigPath -ChildPath 'Module.Config.json'
         $ConfigDefaults = Join-Path -Path $(Split-Path -Path $PSScriptRoot -Parent) -ChildPath "\Config\Module.Defaults.json"
-    } catch {
-        Write-Error $_.Exception.Message
+    }
+    catch {
+        Write-Error "CommandPath: $($CommandPath)`nError: `n$($_)`n Invocation $($_.InvocationInfo.ScriptLineNumber) $($_.InvocationInfo.ScriptName)"
         return
     }
     # Test to see if module config JSON exists and create it if it doesn't
@@ -31,11 +32,18 @@ function Get-ModuleConfig {
         }
         $HashTable.Add('ModuleName', $ModuleName)
         $HashTable.Add('ModulePath', $ModulePath)
-        $HashTable.Add('ModuleConfigPath',$ModuleConfigPath)
-        $HashTable.Add('ModuleConfigFilePath',$ModuleConfigFilePath)
+        $HashTable.Add('ModuleConfigPath', $ModuleConfigPath)
+        $HashTable.Add('ModuleConfigFilePath', $ModuleConfigFilePath)
         Set-ModuleConfig @HashTable
-        Get-ModuleConfig -CommandPath $CommandPath
-    } else {
+        try {
+            Get-ModuleConfig -CommandPath $CommandPath -ErrorAction Stop
+        }
+        catch {
+            Write-Error "CommandPath: $($CommandPath)`nError: `n$($_)`n Invocation $($_.InvocationInfo.ScriptLineNumber) $($_.InvocationInfo.ScriptName)"
+            return
+        }
+    }
+    else {
         $Config = Get-Content -Path $ModuleConfigFilePath | ConvertFrom-Json
         if ($Config.ModulePath -ne $ModulePath) {
             $Config.ModulePath = $ModulePath
