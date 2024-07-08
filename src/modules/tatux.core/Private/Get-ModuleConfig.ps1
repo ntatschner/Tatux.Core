9e68cefd66da6d7c36c1d4ff32134cee277c2808
Nigel Tatschner <ntatschner@gmail.com>
2024-07-08 11:39:05 +0100

    resolving config cmdlet issue

--- src/modules/tatux.core/Private/Get-ModuleConfig.ps1
+++ src/modules/tatux.core/Private/Get-ModuleConfig.ps1
@@ -7,12 +7,18 @@ function Get-ModuleConfig {
         $CommandPath
     )
     try {
-        $ModulePath = $(Split-Path -Path (Split-Path -Path $CommandPath -Parent) -Parent)
+        $ModulePath = Split-Path -Path $CommandPath -Parent
+        Write-Verbose "ModulePath: $ModulePath"
         $ModuleName = Get-ChildItem -Path $ModulePath -Filter "*.psd1" -File | Select-Object -First 1 | Select-Object -ExpandProperty BaseName
+        Write-Verbose "ModuleName: $ModuleName"
         $UserPowerShellModuleConfigPath = Join-Path -Path $(Split-Path -Path $($env:PSModulePath -split ';' | ForEach-Object { if (($_ -match $([regex]::Escape($env:USERNAME))) -and ($_ -notmatch '\.')) { $_ } }) -Parent) -ChildPath 'Config'
+        Write-Verbose "UserPowerShellModuleConfigPath: $UserPowerShellModuleConfigPath"
         $ModuleConfigPath = Join-Path -Path $UserPowerShellModuleConfigPath -ChildPath $ModuleName
+        Write-Verbose "ModuleConfigPath: $ModuleConfigPath"
         $ModuleConfigFilePath = Join-Path -Path $ModuleConfigPath -ChildPath 'Module.Config.json'
+        Write-Verbose "ModuleConfigFilePath: $ModuleConfigFilePath"
         $ConfigDefaults = Join-Path -Path $(Split-Path -Path $PSScriptRoot -Parent) -ChildPath "\Config\Module.Defaults.json"
+        Write-Verbose "ConfigDefaults: $ConfigDefaults"
     }
     catch {
         Write-Error "CommandPath: $($CommandPath)`nError: `n$($_)`n Invocation $($_.InvocationInfo.ScriptLineNumber) $($_.InvocationInfo.ScriptName)"
@@ -21,10 +27,12 @@ function Get-ModuleConfig {
     # Test to see if module config JSON exists and create it if it doesn't
     if (-not (Test-Path -Path $ModuleConfigFilePath)) {
         $DefaultConfig = Get-Content -Path $ConfigDefaults | ConvertFrom-Json
+        Write-Verbose "DefaultConfig: $DefaultConfig"
         $HashTable = @{}
         $DefaultConfig.PSObject.Properties | ForEach-Object { $HashTable[$_.Name] = $_.Value }
         while ($null -eq (Get-ChildItem -Path $ModulePath -Filter "*.psd1" -File)) {
             $ModulePath = Split-Path -Path $ModulePath -Parent
+            Verbose "ModulePath: $ModulePath"
             if ($ModulePath -eq (Split-Path -Path $ModulePath -Parent)) {
                 Write-Error "No .psd1 file found in the directory tree."
                 return
