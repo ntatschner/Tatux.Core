92ccb3174a4faf2492ef7cae3db2e1cac58a6512
Nigel Tatschner <ntatschner@gmail.com>
2024-07-22 14:44:52 +0100

    Adding in Beta Feature Warning

--- src/modules/tatux.core/Private/Get-ModuleStatus.ps1
+++ src/modules/tatux.core/Private/Get-ModuleStatus.ps1
@@ -17,7 +17,7 @@ function Get-ModuleStatus {
             $CurrentlyLoadedModuleVersion = (Import-PowerShellDataFile -Path $PSD1File).ModuleVersion
             $LatestModuleVersion = (Find-Module -Name $ModuleName).Version
             if ($CurrentlyLoadedModuleVersion -lt $LatestModuleVersion) {
-                Write-Host -ForegroundColor Yellow "An update is available for the module '$ModuleName'. Installed version: $CurrentlyLoadedModuleVersion, Latest version: $LatestModuleVersion.`nPlease run 'Update-Module $ModuleName' to update the module."
+                Write-Host -ForegroundColor Yellow "An update is available for the module '$ModuleName'. Installed version: $CurrentlyLoadedModuleVersion, Latest version: $LatestModuleVersion.`nPlease run 'Update-PSResource -Name $ModuleName' to update the module."
             }
             return
         }
